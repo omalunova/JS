9e0fd5c7848086b0e15234d9aa363246868375bc
omalunova <omalunova@gmail.com>
2023-11-21 16:56:58 +0200

    HW1

---
+++ HW1/1.js
@@ -0,0 +1,34 @@
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let S1 = 'hello';
+let S2 = 'owu';
+let S3 = 'com';
+let S4 = 'ua';
+let N1 = 1;
+let N2 = 10;
+let N3 = -999;
+let N4 = 123;
+let N5 = 3.14;
+let N6 = 2.7;
+let N7 = 16;
+let Bool1 = true;
+let Bool2 = false;
+console.log(S1,S2,S3,S4,N1,N2,N3,N4,N5,N6,N7);
+let firstName = 'Malunova ';
+let middleName = 'Oksana ';
+let lastName = 'Oleksandrivna';
+let Person = (firstName+middleName+lastName);
+console.log(Person);
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+let FName = prompt('What is your name?','');
+let MName = prompt('What is your middle name?','');
+let LName = prompt('What is your surname?','');
+console.log(FName + MName + LName);
+
